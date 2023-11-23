4bc4c970aad6934f625771d77eca4146aa568b3b
OforiAlexander <alexanderofori07@gmail.com>
2023-11-23 08:42:22 +0000

    Inital installment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        'sans': 'Work Sans , sans-serif'
+      }
+    },
+  },
+  plugins: [],
+}
+
